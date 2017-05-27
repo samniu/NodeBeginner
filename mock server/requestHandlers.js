d2f6f5007333937fe672f65520be9710661af5aa
Heroic-Niu <nsm1168@126.com>
2017-05-27 10:28:01 +0800

    bug fixed

--- mock server/requestHandlers.js
+++ mock server/requestHandlers.js
@@ -19,6 +19,7 @@ function start(response) {
       '</form>'+
       '</body>'+
       '</html>';
+
       response.writeHead(200, {"Content-Type": "text/html"});
 	  response.write(body);
 	  response.end();
@@ -65,17 +66,17 @@ function upload(response, request) {
 	console.log("about to parse");
 	form.parse(request, function(error, fields, files) {
 		console.log("parsing done");
-		fs.renameSync(files.upload.path, "/Users/niusam/tmp/test.png");
+		fs.renameSync(files.upload.path, "/tmp/test.png");
 		response.writeHead(200, {"Content-Type": "text/html"});
 		response.write("received image:<br/>");
-		response.write("<ima src='/show' />");
+		response.write("<img src='/show' />");
 		response.end();
 	});
 }
 
 function show(response) {
    console.log("Request handler 'show' was called");
-   fs.readFile("/Users/niusam/tmp/test.png", "binary", function(error, file){
+   fs.readFile("/tmp/test.png", "binary", function(error, file){
    	 if(error) {
    	 	response.writeHead(500, {"Content-Type": "text/plain"});
    	 	response.write(error + "\n");
